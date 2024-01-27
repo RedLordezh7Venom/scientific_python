28732f4772e3d97a91d81ef6c0acf9b43698e855
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2024-01-28 04:48:56 +0530

    Create cipher.py

---
+++ assignments/cipher.py
@@ -0,0 +1,3 @@
+'''The first kind of cipher  to build is called a Caesar cipher.
+ take each letter in the message, find its position in the alphabet,
+   take the letter located after 3 positions, and replace the original letter with the new letter.'''
