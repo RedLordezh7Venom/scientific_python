70708c06471242a8877d8e213f5fc86789ec0d4f
lordpotato <54540149+RedLordezh7Venom@users.noreply.github.com>
2024-03-07 00:30:02 +0530

    Update cipher.py

--- assignments/cipher.py
+++ assignments/cipher.py
@@ -1,3 +1,13 @@
 '''The first kind of cipher  to build is called a Caesar cipher.
  take each letter in the message, find its position in the alphabet,
    take the letter located after 3 positions, and replace the original letter with the new letter.'''
+
+text = 'Hello World'
+shift = 3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+'''
+index = alphabet.find(text[0].lower())
+print(index)
+shifted = alphabet[index + shift]
+print(shifted)
+'''
